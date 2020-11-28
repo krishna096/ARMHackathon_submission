32685fa124f018a49891a37ca51ec7eb0e6e6799
krishna096 <72197791+krishna096@users.noreply.github.com>
2020-11-29 00:46:15 +0530

    Update MT2019511_print.c

--- MT2019511_print.c
+++ MT2019511_print.c
@@ -4,26 +4,26 @@ void printMsg(const int a, const int b, const int c)
 {
 	 char Msg[100];
 	 char *ptr;
-	 sprintf(Msg, "Angle  is %d degrees, ", a);
+//	 sprintf(Msg, "Angle  is %d degrees, ", a);
+//	 ptr = Msg ;
+//   while(*ptr != '\0')
+//	 {
+//      ITM_SendChar(*ptr);
+//      ++ptr;
+//   }
+	 sprintf(Msg, " %d, %d \n  ",b,c);
 	 ptr = Msg ;
    while(*ptr != '\0')
 	 {
       ITM_SendChar(*ptr);
       ++ptr;
    }
-	 sprintf(Msg, " x = %d, ", b);
-	 ptr = Msg ;
-   while(*ptr != '\0')
-	 {
-      ITM_SendChar(*ptr);
-      ++ptr;
-   }
-	 	 sprintf(Msg, " y = %d\n", c);
-	 ptr = Msg ;
-   while(*ptr != '\0')
-	 {
-      ITM_SendChar(*ptr);
-      ++ptr;
-	 } 
+//	 	 sprintf(Msg, " y = %d\n", c);
+//	 ptr = Msg ;
+//   while(*ptr != '\0')
+//	 {
+//      ITM_SendChar(*ptr);
+//      ++ptr;
+//	 } 
 		 
 }
